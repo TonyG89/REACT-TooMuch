ee356a20901d8ddb1a987c8c5fd6d688647bfec1
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-09-08 15:39:18 +0300

    Updates

---
+++ precache-manifest.b7966ffb3b02d93ca2d1c9531d4f6bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e582e31a432db178c84",
+    "url": "/Okylistik/TooMuchPlainClothes-react/static/css/main.d51cac6b.chunk.css"
+  },
+  {
+    "revision": "1e582e31a432db178c84",
+    "url": "/Okylistik/TooMuchPlainClothes-react/static/js/main.1e582e31.chunk.js"
+  },
+  {
+    "revision": "2f25a71f3b38b1fed19a",
+    "url": "/Okylistik/TooMuchPlainClothes-react/static/js/1.2f25a71f.chunk.js"
+  },
+  {
+    "revision": "f08af1e648fa37778c88",
+    "url": "/Okylistik/TooMuchPlainClothes-react/static/js/runtime~main.f08af1e6.js"
+  },
+  {
+    "revision": "d3cdd3be81e0270683453cef07602e4b",
+    "url": "/Okylistik/TooMuchPlainClothes-react/index.html"
+  }
+];
