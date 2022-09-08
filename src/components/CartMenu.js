8c52d47efc1fd6b1d174df2002530fc1e48344ab
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-09-08 18:18:23 +0300

    n

--- src/components/CartMenu.js
+++ src/components/CartMenu.js
@@ -1,36 +1,8 @@
 import React from "react";
-import Info from "./Info";
-import axios from "axios";
-import {useTotalSum} from '../hooks/useTotalSum'
-
-const delay = (ms)=> new Promise(resolve=> setTimeout(resolve,ms))
 
 export default function CartMenu(props) {
-  const { items, onClose, onDelete } = props;
-  const [orderId, setOrderId] = React.useState(null);
-  const [orderComplete, setOrderComplete] = React.useState(false);
-  const [btnLoading, setBtnLoading] = React.useState(false);
-  const {setCartClothes,cartClothes, totalPrice} = useTotalSum()
-  
-  const onClickOrder = async () => {
-try {
-  setBtnLoading(false)
-  const {data} = await axios.post("https://630927d6722029d9dddf3c35.mockapi.io/orders", {goods: cartClothes})
-  setOrderId(data.id)
-  setOrderComplete(true);
-  setCartClothes([]);
-
-for (let index = 0; index < cartClothes.length; index++) {
-  const item = cartClothes[index];
-  await axios.delete(`https://630927d6722029d9dddf3c35.mockapi.io/cart/${item.id}`)
-  await delay(1000)
-}
-  
-} catch (error) {
-  alert(`Помилка: ${error}`)
-}
-setBtnLoading(true)
-  };
+  const { items, onClose } = props;
+  const {color, link, name, size, price} = items
 
   const svgRemove = (
     <svg
@@ -46,6 +18,7 @@ setBtnLoading(true)
       />
     </svg>
   );
+  const url = `./img/clothes/${link}.jpg`;
 
   return (
     <div className="overlay">
@@ -55,59 +28,41 @@ setBtnLoading(true)
           <div className="close" onClick={onClose}>
             <img src="img/btn-remove.svg" alt="прибрати" />
           </div>
+          <div className="items">
 
-          {items.length > 0 ? (
-            <>
-              <div className="items">
-                {items.map((item) => (
-                  <div key={item.id} className="card">
-                    <img
-                      maxWidth={210}
-                      src={`./img/clothes/${item.link}.jpg`}
-                      alt="одяг"
-                    />
-                    <div className="card-body">
-                      <div className="card-info">
-                        <p>
-                          {item.name} {item.color} розмір{item.size}
-                        </p>
-                        <b>{item.price} грн</b>
-                      </div>
-                      <div>
-                        <button onClick={() => onDelete(item.id)}>
-                          {svgRemove}
-                        </button>
-                      </div>
-                    </div>
+            {items.map((item) => (
+              <div className="card">
+                <img maxWidth={210} src={`./img/clothes/${item.link}.jpg`} alt="кросівки" />
+                <div className="card-body">
+                  <div className="card-info">
+                    <p>
+                      {item.name} {item.color} розмір{item.size}
+                    </p>
+                    <b>{item.price} грн</b>
                   </div>
-                ))}
+                  <div>
+                    <button>{svgRemove}</button>
+                  </div>
+                </div>
               </div>
-              <div className="cart-menu-bottom">
-                <ul>
-                  <li>
-                    <span>Всього:</span>
-                    <div className="dashed" />
-                    <b>{totalPrice} грн</b>
-                  </li>
-                  <li>
-                    <span>Податок:</span>
-                    <div className="dashed" />
-                    <b>{totalPrice*0.05}грн</b>
-                  </li>
-                </ul>
-                <button className="greenButton" disabled={btnLoading} onClick={onClickOrder}>
-                  {" "}
-                  Замовити!
-                </button>
-              </div>
-            </>
-          ) : (
-            <Info
-              img={ orderComplete ? "complete-order" :"empty-cart"}
-              title={ orderComplete ? "Ваше замовлення оформлене" :"Кошик пустий"}
-              description={ orderComplete ? `Ваше замовлення №${orderId}. З вами в ближайший час зв'яжуться.` :"Додайте щось у кошик, щоб зробити замовлення."}
-            />
-          )}
+              ))}
+
+          </div>
+        </div>
+        <div className="cart-menu-bottom">
+          <ul>
+            <li>
+              <span>Всього:</span>
+              <div className="dashed" />
+              <b>0грн</b>
+            </li>
+            <li>
+              <span>Податок:</span>
+              <div className="dashed" />
+              <b>0грн</b>
+            </li>
+          </ul>
+          <button> Замовити!</button>
         </div>
       </div>
     </div>
