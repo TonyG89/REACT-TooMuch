2df45293fcffdf843d8cf4724d5dec1e9c9d26de
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-12-30 23:16:11 +0200

    new

---
+++ precache-manifest.b71b19b17124733996f4656155e195a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75931b133fc53eaaf575",
+    "url": "/REACT-TooMuch/static/css/main.b45e441a.chunk.css"
+  },
+  {
+    "revision": "75931b133fc53eaaf575",
+    "url": "/REACT-TooMuch/static/js/main.75931b13.chunk.js"
+  },
+  {
+    "revision": "e82e931046385c20b6fa",
+    "url": "/REACT-TooMuch/static/js/1.e82e9310.chunk.js"
+  },
+  {
+    "revision": "54c15fca19a5d66dffac",
+    "url": "/REACT-TooMuch/static/js/runtime~main.54c15fca.js"
+  },
+  {
+    "revision": "0a9b4eae3488f67be9fc322ac02264f1",
+    "url": "/REACT-TooMuch/index.html"
+  }
+];
--- src/hooks/useTotalSum.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppContext from '../context';
-
-export const useTotalSum = () => {
-  const { cartClothes, setCartClothes } = React.useContext(AppContext);
-  const totalPrice = cartClothes.reduce((sum, obj) => obj.price + sum, 0);
-  return { cartClothes, setCartClothes, totalPrice };
-};
