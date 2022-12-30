3cdb8dfc8e4b2d32faf7b53d0893ac7fcdc7b646
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-12-30 15:21:11 +0200

    right path

---
+++ build/precache-manifest.d80c88f5304435818e915e97dbf6ee80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c13bb64ad3f1de053585",
+    "url": "/TonyG89/REACT-TooMuch/static/css/main.ae021d70.chunk.css"
+  },
+  {
+    "revision": "c13bb64ad3f1de053585",
+    "url": "/TonyG89/REACT-TooMuch/static/js/main.c13bb64a.chunk.js"
+  },
+  {
+    "revision": "5589a5e7f516756319fe",
+    "url": "/TonyG89/REACT-TooMuch/static/js/1.5589a5e7.chunk.js"
+  },
+  {
+    "revision": "509b3f89c5773fa500e9",
+    "url": "/TonyG89/REACT-TooMuch/static/js/runtime~main.509b3f89.js"
+  },
+  {
+    "revision": "6960dee295063fa0016388fc634d7995",
+    "url": "/TonyG89/REACT-TooMuch/index.html"
+  }
+];
