d53433eff8995affb5bf24c72d09b6f6ac9c6151
Anton Hryhoriev <102998388+TonyG89@users.noreply.github.com>
2022-12-30 16:24:45 +0200

    Update precache-manifest.a83349827e3c5588f5c7f6c8151c97ef.js

--- precache-manifest.a83349827e3c5588f5c7f6c8151c97ef.js
+++ precache-manifest.a83349827e3c5588f5c7f6c8151c97ef.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
     "revision": "c13bb64ad3f1de053585",
-    "url": "./TonyG89/REACT-TooMuch/static/css/main.ae021d70.chunk.css"
+    "url": "/TonyG89/REACT-TooMuch/static/css/main.ae021d70.chunk.css"
   },
   {
     "revision": "c13bb64ad3f1de053585",
-    "url": "./TonyG89/REACT-TooMuch/static/js/main.c13bb64a.chunk.js"
+    "url": "/TonyG89/REACT-TooMuch/static/js/main.c13bb64a.chunk.js"
   },
   {
     "revision": "5589a5e7f516756319fe",
-    "url": "./TonyG89/REACT-TooMuch/static/js/1.5589a5e7.chunk.js"
+    "url": "/TonyG89/REACT-TooMuch/static/js/1.5589a5e7.chunk.js"
   },
   {
     "revision": "509b3f89c5773fa500e9",
