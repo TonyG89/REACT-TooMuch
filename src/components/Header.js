5f2ec57da287dcce2066581133a877a3abd14edd
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-12-31 00:24:35 +0200

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {useTotalSum} from '../hooks/useTotalSum.js'
+import { useTotalSum } from '../hooks/useTotalSum.js'
 
 export default function Header(props) {
-  const {totalPrice} = useTotalSum()
-  const {onClickCart, onClickFavorites} = props;
+  const { totalPrice } = useTotalSum()
+  const { onClickCart, onClickFavorites } = props;
 
   return (
     <header>
       <div className="headerLeft">
         <Link to="/">
           <img width={40} height={40} src="./img/logo.png" alt="logo tmpc" />
+        </Link>
         <div className="headerInfo">
           <h3>Too Much Plain clothes</h3>
           <p>Найкращий магазин бланкової одежі</p>
         </div>
-        </Link>
       </div>
       <div className="headerRight">
         <div className="cart" onClick={onClickCart}>
           <img width={20} height={20} src="./img/cart.svg" alt="cart" />
           {/* если корзина пуста, убираем цену */}
-          {totalPrice!==0 && <span>{totalPrice} грн</span>}
+          {totalPrice !== 0 && <span>{totalPrice} грн</span>}
         </div>
         <Link to="/favorites">
           <div className="favorites">
@@ -35,9 +35,9 @@ export default function Header(props) {
           </div>
         </Link>
         <Link to="/orders">
-        <div className="user">
-          <img width={20} height={20} src="./img/user.svg" alt="user" />
-        </div>
+          <div className="user">
+            <img width={20} height={20} src="./img/user.svg" alt="user" />
+          </div>
         </Link>
       </div>
     </header>
